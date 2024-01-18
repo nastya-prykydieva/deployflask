dd7523c96dc7edef54141139936919f1e1de25d3
Nastia Prykydieva <nastia.pryk@gmail.com>
2024-01-18 20:33:39 +0200

    deploy preparation

---
+++ config.py
@@ -0,0 +1,38 @@
+from datetime import timedelta
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+DATABASE_URL = "postgresql://db_ez1h_user:E3MUwcuyxlK8eioxTwB0GnrW3LGbKRRC@dpg-cmk80qev3ddc738q9tlg-a.frankfurt-postgres.render.com/db_ez1h"
+
+
+class Config(object):
+    DEBUG = False
+    DEVELOPMENT = False
+    WTF_CSRF_ENABLED = True
+    SECRET_KEY = 'secret'
+    FLASK_SECRET = SECRET_KEY
+    JWT_SECRET_KEY = 'b8dad7b10bba86fe6ed93069'
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=1)
+
+
+class DevConfig(Config):
+    DEBUG = True
+    DEVELOPMENT = True
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test_db.sqlite'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    WTF_CSRF_ENABLED = False
+
+
+config = {
+    'dev': DevConfig,
+    'test': TestConfig,
+    'default': DevConfig,
+}
